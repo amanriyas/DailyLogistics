15c38c051c58e887dd2961a7d66605abe9bea407
amanriyas <153854025+amanriyas@users.noreply.github.com>
2024-12-23 14:27:14 +0530

    need further testing

--- attendance/serializer.py
+++ attendance/serializer.py
@@ -4,26 +4,26 @@ from .models import *
 class AllEmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = ['name', 'role_id', 'join_date']
+        fields = "__all__"
 
 class AllRoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
-        fields = ['role_name', 'role_description', 'hourly_wage']
+        fields = "__all__"
 
 class AllSiteSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Sites
-        fields = ['site_name', 'site_location', 'site_status']
+        fields = "__all__"
         
 
 class SiteAttendanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = SiteAttendance
-        fields = ['employee_id', 'site_id', 'date', 'no_of_hours', 'status']
+        fields = "__all__"
 
 
 class EmployeeAttendanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = EmployeeAttendance
-        fields = ['employee_id', 'site_id', 'date', 'status']
+        fields = "__all__"
--- attendance/views.py
+++ attendance/views.py
@@ -17,12 +17,7 @@ class AllEmployeesList(APIView):
     def get (self, request):
        all_employees = Employee.objects.all()
        serializer = AllEmployeeSerializer(all_employees, many=True)
-       sample_employee = {
-        "name": "John Doe",
-        "role_id": 1,
-        "join_date": "2020-01-01"
-        }
-       return JsonResponse(sample_employee, status=status.HTTP_200_OK)
+       return Response(serializer.data, status=status.HTTP_200_OK)
     
 class AddEmployee(APIView):
     def post(self, request):
@@ -30,7 +25,11 @@ class AddEmployee(APIView):
         serializer = AllEmployeeSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+            response_data = {
+                "message": "Employee added successfully",
+                "data": serializer.data
+            }
+            return JsonResponse(response_data, status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
 
 class EditEmployee(APIView):
@@ -60,15 +59,7 @@ class AllRoles(APIView):
     def get (self, request):
        all_roles = Role.objects.all()
        serializer = AllRoleSerializer(all_roles, many=True)
-       
-       sample_role = {
-        "role_name": "Manager",
-        "role_description": "Manages the site",
-        "hourly_wage": 1000
-        }
-
-
-       return JsonResponse(sample_role, status=status.HTTP_200_OK)    
+       return Response(serializer.data, status=status.HTTP_200_OK)    
 
 class AddRole(APIView):
     def post(self, request):
@@ -76,7 +67,11 @@ class AddRole(APIView):
         serializer = AllRoleSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+            response_date = {
+                "message": "Role added successfully",
+                "data": serializer.data
+            }
+            return JsonResponse(response_date, status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
     
 class EditRole(APIView):
@@ -93,9 +88,9 @@ class EditRole(APIView):
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
     
 class DeleteRole(APIView):
-    def delete(self, request, pk):
-        role = self.get_object(pk)
-        role.delete()
+    def delete(self, request,id):
+        role = Roles.objects.get(pk=pk)
+        AllRoles.objects.filter(pk=role).delete()
         return JsonResponse({"message": "Role deleted successfully"}, status=status.HTTP_204_NO_CONTENT)    
     
 ############# Site Views ####################
@@ -103,20 +98,19 @@ class AllSites(APIView):
     def get (self, request):
        all_sites = Sites.objects.all()
        serializer = AllSiteSerializer(all_sites, many=True)
-       
-       sample_site = {
-        "site_name": "Site 1",
-        "site_location": "Location 1",
-        "site_status": "Active"
-        }
-       return JsonResponse(sample_site, status=status.HTTP_200_OK)  
+       return Response(serializer.data, status=status.HTTP_200_OK)  
 
 class AddSite(APIView):
     def post(self, request):
         data = request.data
         serializer = AllSiteSerializer(data=data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save() 
+            response_date = {
+                "message": "Site added successfully",
+                "data": serializer.data
+            }
+
             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)       
 
@@ -144,28 +138,13 @@ class SiteAttendanceList(APIView):
     def get (self, request):
        all_site_attendance = SiteAttendance.objects.all()
        serializer = SiteAttendanceSerializer(all_site_attendance, many=True)
-       
-       sample_site_attendance = {
-        "employee_id": 1,
-        "site_id": 1,
-        "date": "2020-01-01",
-        "no_of_hours": 8,
-        "status": "Present"
-        }
-       return JsonResponse(sample_site_attendance, status=status.HTTP_200_OK)
+       return Response(serializer.data, status=status.HTTP_200_OK)
     
 class EmployeeAttendanceList(APIView):
     def get (self, request):
        all_employee_attendance = EmployeeAttendance.objects.all()
        serializer = EmployeeAttendanceSerializer(all_employee_attendance, many=True)
-       
-       sample_employee_attendance = {
-        "employee_id": 1,
-        "site_id": 1,
-        "date": "2020-01-01",
-        "status": "Present"
-        }
-       return JsonResponse(sample_employee_attendance, status=status.HTTP_200_OK)    
+       return JsonResponse(serializer.data, status=status.HTTP_200_OK)    
     
 
 
